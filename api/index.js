58457d5357fa5ff99fa79f2c60e8d869a6dd466a
Lucas Scariot <lucas@scariot.fr>
2018-07-09 11:02:58 +0200

    Small fix

--- api/index.js
+++ api/index.js
@@ -8,61 +8,66 @@ let users = []
 const messages = []
 
 server = app.listen(8080, function () {
-    console.log('server is running on port 8080')
+  console.log('server is running on port 8080')
 });
 
-io = socket(server);
+io = socket(server, {
+  serveClient: false,
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
 
 
 io.on('connection', (socket) => {
-    users.forEach(user => {
-        socket.emit('USER_JOINED', user)
+  users.forEach(user => {
+    socket.emit('USER_JOINED', user)
+  })
+
+  socket.on('ADD_USER', function (data) {
+    const newUser = {
+      id: socket.id,
+      username: data,
+      isTyping: false,
+      isConnected: true,
+      color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+    }
+
+    messages.forEach(message => {
+      socket.emit('RECEIVE_MESSAGE', message)
     })
 
-    socket.on('ADD_USER', function (data) {
-        const newUser = {
-            id: socket.id,
-            username: data,
-            isTyping: false,
-            isConnected: true,
-            color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
-        }
+    users.push(newUser)
+    io.emit('USER_JOINED', newUser);
+    socket.emit('WELCOME_USER', newUser);
+  })
 
-        messages.forEach(message => {
-            socket.emit('RECEIVE_MESSAGE', message)
-        })
+  socket.on('SEND_MESSAGE', function (data) {
+    if (messages.length >= 10) {
+      messages.splice(0, 1)
+    }
 
-        users.push(newUser)
-        io.emit('USER_JOINED', newUser);
-        socket.emit('WELCOME_USER', newUser);
-    })
+    messages.push(data)
 
-    socket.on('SEND_MESSAGE', function (data) {
-        if (messages.length >= 10) {
-            messages.splice(0, 1)
-        }
+    io.emit('RECEIVE_MESSAGE', data);
+  })
 
-        messages.push(data)
+  socket.on('STARTED_TYPING', () => {
+    clearTimeout(timeOutTyping)
 
-        io.emit('RECEIVE_MESSAGE', data);
-    })
+    socket.broadcast.emit('USER_STARTED_TYPING', socket.id);
 
-    socket.on('STARTED_TYPING', () => {
-        clearTimeout(timeOutTyping)
+    timeOutTyping = setTimeout(() => {
+      socket.broadcast.emit('USER_STOPPED_TYPING', socket.id);
+    }, 2000)
+  })
 
-        socket.broadcast.emit('USER_STARTED_TYPING', socket.id);
+  socket.on('STOPPED_TYPING', () => {
+    socket.broadcast.emit('USER_STOPPED_TYPING', socket.id);
+  })
 
-        timeOutTyping = setTimeout(() => {
-            socket.broadcast.emit('USER_STOPPED_TYPING', socket.id);
-        }, 2000)
-    })
-
-    socket.on('STOPPED_TYPING', () => {
-        socket.broadcast.emit('USER_STOPPED_TYPING', socket.id);
-    })
-
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('USER_LEFT', socket.id);
-        users = users.filter(user => user.id !== socket.id)
-    })
+  socket.on('disconnect', () => {
+      socket.broadcast.emit('USER_LEFT', socket.id);
+      users = users.filter(user => user.id !== socket.id)
+  })
 });
